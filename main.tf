edaf847ad4223efc39322b2124179ea87c4f7be6
Yousif Kahn <yousifkahn862@gmail.com>
2020-06-12 19:19:46 +0200

    create dynamodb table

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "simple_table" {
+  name           = "simple-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
+}
