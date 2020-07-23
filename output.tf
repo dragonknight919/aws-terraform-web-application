bf636b361781a5c1b800322926d8a4bd6a0795c6
Yousif Kahn <yousifkahn862@gmail.com>
2020-07-23 15:53:39 +0200

    enable content serving via cloudfront

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "api_url" {
 output "website_endpoint" {
   value = aws_s3_bucket.minimal_frontend_bucket.website_endpoint
 }
+
+output "cloudfront_endpoint" {
+  value = aws_cloudfront_distribution.minimal_distribution.domain_name
+}
+
+output "name" {
+  value = aws_s3_bucket.minimal_frontend_bucket.bucket_regional_domain_name
+}
--- resources_front_end.tf
+++ resources_front_end.tf
@@ -15,9 +15,52 @@ resource "aws_s3_bucket_object" "minimal_index" {
 resource "aws_s3_bucket_object" "minimal_script" {
   bucket = aws_s3_bucket.minimal_frontend_bucket.id
   key    = "index.js"
+  acl    = "public-read"
+
   content = templatefile(
     "./resources_front_end/index.js",
-    { api_url = aws_api_gateway_deployment.minimal.invoke_url }
+    {
+      api_url = aws_api_gateway_deployment.minimal.invoke_url
+    }
   )
-  acl = "public-read"
+}
+
+resource "aws_cloudfront_distribution" "minimal_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.minimal_frontend_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.minimal_frontend_bucket.id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.minimal_frontend_bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 900
+    max_ttl                = 3600
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
