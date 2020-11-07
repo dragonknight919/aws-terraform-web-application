68c0efc1a932f4bdb69aeb421704095db5842e7d
Yousif Kahn <yousifkahn862@gmail.com>
2020-11-07 15:54:16 +0100

    separate index from app front in front end

--- resources_front_end.tf
+++ resources_front_end.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "minimal_frontend_bucket" {
   acl = var.insecure ? "public-read" : "private"
 }
 
-resource "aws_s3_bucket_object" "minimal_index" {
+resource "aws_s3_bucket_object" "index" {
   bucket       = aws_s3_bucket.minimal_frontend_bucket.id
   key          = "index.html"
   content_type = "text/html"
@@ -12,13 +12,22 @@ resource "aws_s3_bucket_object" "minimal_index" {
   content = file("./resources_front_end/index.html")
 }
 
-resource "aws_s3_bucket_object" "minimal_script" {
+resource "aws_s3_bucket_object" "app_html" {
+  bucket       = aws_s3_bucket.minimal_frontend_bucket.id
+  key          = "app.html"
+  content_type = "text/html"
+  acl          = var.insecure ? "public-read" : "private"
+
+  content = file("./resources_front_end/app.html")
+}
+
+resource "aws_s3_bucket_object" "app_script" {
   bucket = aws_s3_bucket.minimal_frontend_bucket.id
-  key    = "index.js"
+  key    = "app.js"
   acl    = var.insecure ? "public-read" : "private"
 
   content = templatefile(
-    "./resources_front_end/index.js",
+    "./resources_front_end/app.js",
     {
       api_url = aws_api_gateway_deployment.minimal.invoke_url
     }
