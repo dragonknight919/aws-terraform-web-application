be16694ec7b33f3c8222bb24a931a69792428c2f
Yousif Kahn <yousifkahn862@gmail.com>
2021-02-04 19:30:19 +0100

    change dynamodb table from provisioned to pay per request billing mode

--- modules/app_back_end/resources_stateful.tf
+++ modules/app_back_end/resources_stateful.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "main" {
   name           = var.resource_name
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
+  billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
 
   attribute {
