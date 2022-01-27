de858c7a2448c2f7998ced0dabb821d91a9fdd8f
Yousif Kahn <yousifkahn862@gmail.com>
2022-01-27 15:22:36 +0100

    add getting items from and posting items to tables to javascript client library

--- modules/crud_app_front_end/content/templates/api_client_library.js
+++ modules/crud_app_front_end/content/templates/api_client_library.js
@@ -3,4 +3,29 @@ export const crudApiTables = ${ crud_api_tables };
 export const crudApiUrl = "${crud_api_url}";
 export const crudApiKey = "${crud_api_key}";
 export const textractApiUrl = "${textract_api_url}";
-export const imageUploadBucketUrl = "${image_upload_bucket_url}"
+export const imageUploadBucketUrl = "${image_upload_bucket_url}";
+
+export async function getTableEntries(table = crudApiTables[0]) {
+  const response = await fetch(crudApiUrl + table, {
+    headers: { "x-api-key": crudApiKey }
+  });
+  if (!response.ok) {
+    throw new Error("HTTP error! status: " + response.status);
+  };
+  return response.json();
+};
+
+export async function createOrDeleteTableEntries(table = crudApiTables[0], operation = "put", entries = []) {
+  const response = await fetch(crudApiUrl + table, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": crudApiKey
+    },
+    body: JSON.stringify({ "operation": operation, "items": entries })
+  });
+  if (!response.ok) {
+    throw new Error("HTTP error! status: " + response.status);
+  };
+  return response.json();
+};
