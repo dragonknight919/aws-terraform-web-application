c434fd5141ac0a1efd8fd09a433a409c929bc82a
Yousif Kahn <yousifkahn862@gmail.com>
2022-09-06 20:28:46 +0200

    refactor html url redirection to resources without extensions

--- modules/crud_app_front_end/content/redirect_missing_file_extension_to_html.js
+++ modules/crud_app_front_end/content/redirect_missing_file_extension_to_html.js
@@ -2,16 +2,21 @@ function handler(event) {
   var request = event.request;
   var uri = request.uri;
 
-  if (uri == '/') {
-    // This function overrides the default root object, so this is here to make up for that.
-    request.uri = '/index.html';
-  } else if (uri.endsWith('/') || uri.endsWith('.')) {
-    request.uri = uri.slice(0, -1).toLowerCase() + '.html';
-  } else if (!uri.includes('.')) {
-    request.uri = uri.toLowerCase() + '.html';
-  } else {
-    request.uri = uri.toLowerCase();
+  if (uri.endsWith('.html')) {
+    uri = uri.slice(0, -5);
   }
+  if (uri.endsWith('index')) {
+    uri = uri.slice(0, -5);
+  }
+  if (uri.endsWith('/')) {
+    uri = uri.slice(0, -1);
+  }
+  // This function overrides the default root object, so this is here to make up for that.
+  if (uri == '') {
+    uri = '/index.html';
+  }
+
+  request.uri = uri;
 
   return request;
 }
